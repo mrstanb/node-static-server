5ec4ef99f80fafc489fe6694e7720c08aafd0b07
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-03-18 19:12:54 +0100

    Use Content-Disposition of type "attachment"

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 5050;
 
-app.use(express.static("public"));
+app.use(
+  express.static("public", {
+    setHeaders: (res, path) => {
+      res.set("Content-Disposition", "attachment");
+    },
+  })
+);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
